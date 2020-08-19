4c9d5e7753eec1d7e5df7347ccea07719c647417
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2020-08-19 15:48:51 -0500

    initial configuration to the project

---
+++ rouletteGambling/Controllers/RouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rouletteGambling.Controllers
+{
+    [ApiController]
+    [Route("roulette")]
+    public class RouletteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getroulette")]
+        public ActionResult GetRoulette()
+        {
+            return Ok("ok");
+        }
+    }
+}
