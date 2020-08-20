cd3f554513d5d9531534caf931820deeb82ca291
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2020-08-19 20:17:26 -0500

    was created the models and enums files

---
+++ rouletteGambling/Models/GamblerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rouletteGambling.Models
+{
+    public class GamblerModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Credits { get; set; }
+    }
+}
---
+++ rouletteGambling/Utils/Enums/StatusRouletteEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rouletteGambling.Utils.Enums
+{
+    public enum StatusRouletteEnum
+    {
+        OPEN = 1,
+        CLOSE = 0
+    }
+}
